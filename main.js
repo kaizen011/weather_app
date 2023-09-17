f377262f9ac45d3622b52e7f3df7ef06d3420c32
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-09-17 13:00:29 -0500

    Fully functioning search bar to get temp value

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const dataChild = document.getElementById('dyshi');
 
-async function getWeather() {
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+
+searchButton.addEventListener('click',()=>{
+    const searchTerm = searchInput.value.trim();
+    if(searchTerm !== ''){
+        searchCity(searchTerm);
+    }
+})
+
+
+async function searchCity(query){
     try {
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=Pristina&aqi=yes', { mode: 'cors' });
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=${query}&aqi=yes`, { mode: 'cors' });
         const weatherData = await response.json();
         dataChild.textContent = weatherData.current.temp_c;
     } catch (error) {
@@ -10,4 +22,4 @@ async function getWeather() {
     }
 }
 
-getWeather();
+
