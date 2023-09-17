ad78686a545b621108284fbb0e0c3bbe3ecc5dab
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-09-17 11:29:27 -0500

    Functioning Weather API for Pristina query

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('T')
+const dataChild = document.getElementById('dyshi');
+
+async function getWeather() {
+    try {
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=Pristina&aqi=yes', { mode: 'cors' });
+        const weatherData = await response.json();
+        dataChild.textContent = weatherData.current.temp_c;
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+getWeather();
