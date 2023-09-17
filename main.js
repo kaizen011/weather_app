5ed1993eacc811fdb463f198ce7e1e94f79e009c
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-09-17 14:50:46 -0500

    Continuing background feature tomorrow

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataChild = document.getElementById('dyshi');
-
+const body = document.getElementsByClassName('body');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
@@ -17,9 +17,16 @@ async function searchCity(query){
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=${query}&aqi=yes`, { mode: 'cors' });
         const weatherData = await response.json();
         dataChild.textContent = weatherData.current.temp_c;
+        dataChild.textContent = dataChild.textContent + "C " + weatherData.current.temp_f + "F";
     } catch (error) {
         console.error('An error occurred:', error);
     }
+    return dataChild;
 }
 
 
+// Changing image background in relation to weather temperature
+if (dataChild < 20){
+    const img = document.getElementById();
+    body.appendChild()
+}
