a23ed3b570f5b931ebdf510e3be734d52eeadad3
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-09-19 12:38:59 -0500

    Working gjakova file

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const dataChild2= document.getElementById('treshi');
 const body = document.getElementsByClassName('body');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
+const gjakovaData = document.getElementById('weather-text');
+const gjakovaBtn = document.getElementById('get-gjakova');
+const cond = document.getElementById('cond');
 
 
 searchButton.addEventListener('click',()=>{
@@ -15,11 +18,12 @@ searchButton.addEventListener('click',()=>{
 
 async function searchCity(query){
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=${query}&aqi=yes`, { mode: 'cors' });
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b68fbe11005642c2973171623231909&s&q=${query}&aqi=yes`, { mode: 'cors' });
         const weatherData = await response.json();
         dataChild.textContent = weatherData.current.temp_c;
         dataChild.textContent = "Degrees Celsius:  " + dataChild.textContent + "°C";
-        dataChild2.textContent = "Fahrenheit: " + weatherData.current.temp_f + "°F";
+        dataChild2.textContent = "Condition" + weatherData.condition.text;
+        
     } catch (error) {
         console.error('An error occurred:', error);
     }
@@ -28,25 +32,41 @@ async function searchCity(query){
 
 
 
+gjakovaBtn.addEventListener('click', () => {
+    gjakova(); // Call the initial fetch for Gjakova when the button is clicked
+    fetchWeatherPeriodically(); // Start the periodic fetching loop
+});
 
+async function gjakova() {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b68fbe11005642c2973171623231909&s&q=Gjakova&aqi=yes`, { mode: 'cors' });
+        const weatherDataGK = await response.json();
+        gjakovaData.textContent = "Degrees Celsius: " + weatherDataGK.current.temp_c + "°C";
+        dataChild2.textContent = "Weather conditions are: " + weatherDataGK.current.condition.text;
+        if(dataChild2 == "Partly cloudy"){
+            
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
 
+function fetchWeatherPeriodically() {
+    let count = 0;
+    const totalFetches = 96;
+    const interval = 15 * 60 * 1000; // 15 minutes in milliseconds
 
-// for(let i = 1; i > 97; i++){
-//     async function gjakova(){
-//         try {
-//             const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=Gjakova&aqi=yes`, { mode: 'cors' });
-//             const weatherData = await response.json();
-//             dataChild.textContent = weatherData.current.temp_c;
-//             dataChild.textContent = "Degrees Celsius:  " + dataChild.textContent + "°C";
-//             dataChild2.textContent = "Fahrenheit: " + weatherData.current.temp_f + "°F";
-//         } catch (error) {
-//             console.error('An error occurred:', error);
-//         }
-//         return dataChild;
-//     }
-    
+    async function fetchAndSchedule() {
+        await gjakova();
+        count++;
 
+        if (count < totalFetches) {
+            setTimeout(fetchAndSchedule, interval);
+        }
+    }
 
+    fetchAndSchedule();
+}
    //API Gjakova https://api.weatherapi.com/v1/current.json?key=8caf1fc5914a41f5a9a161626231709&s&q=Gjakova&aqi=yes
 // }
 // Changing image background in relation to weather temperature
@@ -56,4 +76,4 @@ async function searchCity(query){
 // if (dataChild < 20){
 //     const img = document.getElementById();
 //     body.appendChild()
-// }
+// 
